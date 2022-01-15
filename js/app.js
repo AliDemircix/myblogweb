8fb76995ffcf45ba27db132ac801d43a722b4e0a
AliDemircix <alidemircix@gmail.com>
2022-01-16 00:25:59 +0100

    news and footer sections built with js dom

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
-import categories from './components/data/categories.js';
 import { topContainerWrapper } from './components/view/topContainer.js';
 import { headerWrapper } from './components/view/header.js';
 import { newPostsWrapper } from './components/view/newPosts.js';
 import { likedPostsWrapper } from './components/view/mostLikedPosts.js';
+import { newsWrapper } from './components/view/news.js';
+import { footer } from './components/view/footer.js';
 const myMobile = document.querySelector('.mobile');
 myMobile.appendChild(headerWrapper);
 myMobile.appendChild(topContainerWrapper);
 myMobile.appendChild(newPostsWrapper);
 myMobile.appendChild(likedPostsWrapper);
+myMobile.appendChild(newsWrapper);
+myMobile.appendChild(footer);
--- js/components/data/posts.js
+++ js/components/data/posts.js
@@ -2,14 +2,11 @@ const posts = [
   {
     id: 'p--1',
     title: 'HTML5 Crash Course',
-    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus sapiente sint architecto tempore dolor fuga cupiditate delectus eveniet ducimus sequi fugit dolore optio et nesciunt earum quos, consequuntur ex soluta.
-  Quaerat necessitatibus distinctio ex architecto inventore temporibus cupiditate. Labore quidem dolorem similique blanditiis ipsum, sequi ut quaerat minus illo impedit? Molestiae ex neque pariatur omnis sequi fugiat quas officia soluta?
-  Blanditiis iste dolores ullam tenetur quisquam consectetur a repudiandae odio eligendi minus explicabo, est impedit nesciunt fugiat iure, dolore, aspernatur molestiae commodi? Itaque molestiae provident sapiente eaque explicabo maiores architecto!
-  Nemo fugit iure fuga, quas esse error? Animi eveniet harum, qui repellat ratione provident obcaecati neque possimus? Dolores sequi nisi voluptates facilis maiores itaque neque nostrum consectetur, consequatur velit nobis.
-  Iure eveniet unde, dignissimos consectetur atque a quibusdam possimus sed nobis iusto, suscipit, ducimus autem sit? Recusandae assumenda tempore odit asperiores! Maiores perferendis iusto, quas optio ut dolorem ipsa maxime?`,
+    content: `HTML5 is a markup language used for structuring and presenting content on the World Wide Web. It is the fifth and last major HTML version that is a World Wide Web Consortium recommendation. The current specification is known as the HTML Living Standard`,
     img: 'img/html.png',
     isFeatured: true,
     isPinned: false,
+    isNews: false,
     writerId: 'w--1',
     writerAvatar: 'img/energy.png',
     writerName: 'AliD.',
@@ -22,9 +19,10 @@ const posts = [
   Blanditiis iste dolores ullam tenetur quisquam consectetur a repudiandae odio eligendi minus explicabo, est impedit nesciunt fugiat iure, dolore, aspernatur molestiae commodi? Itaque molestiae provident sapiente eaque explicabo maiores architecto!
   Nemo fugit iure fuga, quas esse error? Animi eveniet harum, qui repellat ratione provident obcaecati neque possimus? Dolores sequi nisi voluptates facilis maiores itaque neque nostrum consectetur, consequatur velit nobis.
   Iure eveniet unde, dignissimos consectetur atque a quibusdam possimus sed nobis iusto, suscipit, ducimus autem sit? Recusandae assumenda tempore odit asperiores! Maiores perferendis iusto, quas optio ut dolorem ipsa maxime?`,
-    img: 'img/post4.jpg',
+    img: 'img/rapidcss.jpg',
     isFeatured: false,
     isPinned: true,
+    isNews: true,
     writerId: 'w--2',
     writerAvatar: 'img/post4.jpg',
     writerName: 'Rob.',
@@ -37,9 +35,10 @@ const posts = [
   Blanditiis iste dolores ullam tenetur quisquam consectetur a repudiandae odio eligendi minus explicabo, est impedit nesciunt fugiat iure, dolore, aspernatur molestiae commodi? Itaque molestiae provident sapiente eaque explicabo maiores architecto!
   Nemo fugit iure fuga, quas esse error? Animi eveniet harum, qui repellat ratione provident obcaecati neque possimus? Dolores sequi nisi voluptates facilis maiores itaque neque nostrum consectetur, consequatur velit nobis.
   Iure eveniet unde, dignissimos consectetur atque a quibusdam possimus sed nobis iusto, suscipit, ducimus autem sit? Recusandae assumenda tempore odit asperiores! Maiores perferendis iusto, quas optio ut dolorem ipsa maxime?`,
-    img: 'img/js.jpg',
+    img: 'img/rapidjs.jpg',
     isFeatured: false,
     isPinned: true,
+    isNews: true,
     writerId: 'w--3',
     writerAvatar: 'img/post3.jpg',
     writerName: 'Wouter',
@@ -52,12 +51,29 @@ const posts = [
   Blanditiis iste dolores ullam tenetur quisquam consectetur a repudiandae odio eligendi minus explicabo, est impedit nesciunt fugiat iure, dolore, aspernatur molestiae commodi? Itaque molestiae provident sapiente eaque explicabo maiores architecto!
   Nemo fugit iure fuga, quas esse error? Animi eveniet harum, qui repellat ratione provident obcaecati neque possimus? Dolores sequi nisi voluptates facilis maiores itaque neque nostrum consectetur, consequatur velit nobis.
   Iure eveniet unde, dignissimos consectetur atque a quibusdam possimus sed nobis iusto, suscipit, ducimus autem sit? Recusandae assumenda tempore odit asperiores! Maiores perferendis iusto, quas optio ut dolorem ipsa maxime?`,
-    img: 'img/css.jpg',
+    img: 'img/rapidreact.jpg',
     isFeatured: false,
     isPinned: false,
+    isNews: true,
     writerId: 'w--1',
     writerAvatar: 'img/css.jpg',
     writerName: 'Ali D.',
+  },
+  {
+    id: 'p--5',
+    title: 'Learn Wordpress Tutorial',
+    content: `Labore lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus sapiente sint architecto tempore dolor fuga cupiditate delectus eveniet ducimus sequi fugit dolore optio et nesciunt earum quos, consequuntur ex soluta.
+  Quaerat necessitatibus distinctio ex architecto inventore temporibus cupiditate. Labore quidem dolorem similique blanditiis ipsum, sequi ut quaerat minus illo impedit? Molestiae ex neque pariatur omnis sequi fugiat quas officia soluta?
+  Blanditiis iste dolores ullam tenetur quisquam consectetur a repudiandae odio eligendi minus explicabo, est impedit nesciunt fugiat iure, dolore, aspernatur molestiae commodi? Itaque molestiae provident sapiente eaque explicabo maiores architecto!
+  Nemo fugit iure fuga, quas esse error? Animi eveniet harum, qui repellat ratione provident obcaecati neque possimus? Dolores sequi nisi voluptates facilis maiores itaque neque nostrum consectetur, consequatur velit nobis.
+  Iure eveniet unde, dignissimos consectetur atque a quibusdam possimus sed nobis iusto, suscipit, ducimus autem sit? Recusandae assumenda tempore odit asperiores! Maiores perferendis iusto, quas optio ut dolorem ipsa maxime?`,
+    img: 'img/wp.jpg',
+    isFeatured: false,
+    isPinned: false,
+    isNews: true,
+    writerId: 'w--5',
+    writerAvatar: 'img/js.jpg',
+    writerName: ' Eric',
   },
 ];
 export default posts;
