c872a4257f99d1cdd197c74319ab96fce4fe7a3e
AliDemircix <alidemircix@gmail.com>
2022-01-16 02:36:06 +0100

    Like post endless like bug fixed

--- js/components/helper/likeCheck.js
+++ js/components/helper/likeCheck.js
@@ -3,9 +3,16 @@ const likeCheck = () => {
 
   likes.forEach((like) =>
     like.addEventListener('click', (e) => {
-      e.target.innerText = parseInt(e.target.innerText) + 1;
-      console.log(e.target.innerText);
-      e.target.style.fontWeight = 'bolder';
+      if (e.target.classList.contains('liked')) {
+        e.target.innerText = parseInt(e.target.innerText) - 1;
+        e.target.style.fontWeight = 'lighter';
+        e.target.classList.remove('liked');
+      } else {
+        e.target.innerText = parseInt(e.target.innerText) + 1;
+        console.log(e.target.innerText);
+        e.target.style.fontWeight = 'bolder';
+        e.target.classList.add('liked');
+      }
     }),
   );
 };
