43915ace242737ee6dd66eac6a652da273608e99
AliDemircix <alidemircix@gmail.com>
2022-01-20 01:28:15 +0100

    footer subscribe email added without fuction

--- js/components/view/footer.js
+++ js/components/view/footer.js
@@ -1,6 +1,14 @@
 const footer = document.createElement('div');
 footer.className =
-  'container footer tomato-progress d-flex justify-content-center';
-footer.innerHTML = ` <p class="d-flex align-items-end text-white ">Designed by Ali Demirci @2022</p>`;
+  'container footer tomato-progress d-flex justify-content-center  flex-wrap text-center';
+footer.innerHTML = ` 
+<form class="col-lg-12 row mt-3 d-flex justify-content-center mt-4">
+  <div class="form-group col-lg-8 d-flex justify-content-center">
+    <input type="email" class="form-control col-lg-9" id="exampleInputEmail1"  placeholder="Subscribe newsletter">
+    <button type="submit" class="btn btn-dark">Send</button>
+  </div>
+ 
+</form>
+<p class=" col-lg-12 text-white ">Designed by Ali Demirci @2022</p>`;
 
 export { footer };
